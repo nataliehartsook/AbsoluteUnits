cb33c1e47da85322373a18830422ada515d58780
SeanREdwards <edwards.897@osu.edu>
2018-10-27 13:41:44 -0400

    Added main

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main method.
+ *
+ * @param args
+ *            the command line arguments
+ * @throws FileNotFoundException
+ *             in the even the users argument file is not found
+ */
+public static void main(String[] args) throws FileNotFoundException {
+
+}
